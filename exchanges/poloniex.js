24b6b1f248a2755547c7610c4584510d8a79a0ff
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2017-07-27 21:46:10 -0700

    [include] David's code for poloniex

--- exchanges/poloniex.js
+++ exchanges/poloniex.js
@@ -1,3 +1,74 @@
+//DAVES CODE
+require("dotenv").load();
+const Poloniex = require("poloniex.js");
+const axios = require("axios");
+const poloniex = new Poloniex(process.env.POLO_PUB, process.env.POLO_PRIV);
+
+module.exports.btcAddress = "1Mq2vRnyWnsL2DMKbeDsYbbv3npKHCxiVK";
+module.exports.ethAddress = "0xc9d1d6bdfc14837ab576e24a7fcba6e624768583";
+
+module.exports.requestBalances = () => {
+  return new Promise((resolve, reject) => {
+    poloniex.returnBalances((err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+module.exports.buy = (currency, price) => {
+  currency = currency.toUpperCase();
+  module.exports.requestBalances().then(balancesObj => {
+    const pairs = {
+      ETH: {
+        buy: () =>
+          poloniex.buy("BTC", currency, price, balancesObj.BTC / price, (err, data) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log(data);
+              }
+            })
+      },
+      BTC: {
+        buy: () => poloniex.sell(currency, "ETH", price, balancesObj.ETH, (err, data) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log(data);
+              }
+            })
+      }
+    };
+    pairs[currency].buy();
+  });
+};
+
+module.exports.withdraw = (currency, exchange) => {
+  currency = currency.toUpperCase();
+  module.exports.requestBalances().then(balancesObj => {
+    poloniex.withdraw(
+      currency,
+      balancesObj[currency],
+      exchange[currency.toLowerCase() + "Address"],
+      (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(data);
+        }
+      }
+    );
+  });
+};
+
+
+
+
+//CODE FROM ALEX
 require("dotenv").load();
 const btoa = require("btoa");
 const createHmac = require("create-hmac");
@@ -7,7 +78,6 @@ let loopConditional = require('../index').loopConditional;
 const Poloniex = require('poloniex-api-node');
 let poloniex = new Poloniex(process.env.POLONIEX_API_KEY, process.env.POLONIEX_SECRET, { socketTimeout: 15000 });
 let cryptoSocket = require("crypto-socket");
-
 
 const API_KEYS = {
   poloniex: {
@@ -158,10 +228,8 @@ createNewPoloniexCurrencyAddress = (currency) => {
 }
 
 module.exports = {
-  buyEthOnPoloniex,
-  buyBtcOnPoloniex,
-  withdrawEthOnPoloniex,
-  withdrawBtcOnPoloniex,
+  buyOnPoloniex,
+  withdrawFromPoloniex,
   returnBalancesPoloniex,
   returnPoloAddresses,
   createNewPoloniexCurrencyAddress
